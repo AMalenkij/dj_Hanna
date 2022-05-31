0439faacb72da180c6947ab6139dfca180f47fce
Anton Malenkij <itottoro@gmail.com>
2022-06-01 00:36:16 +0200

    refacotr host

--- Hanna/config/settings/ prod.py
+++ Hanna/config/settings/ prod.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['dj-hanna.herokuapp.com']
 
 # Heroku Settings
 django_on_heroku.settings(locals(), staticfiles=False)
