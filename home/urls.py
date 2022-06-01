62a674805bf6cff021aa3a596ff20c845d43095f
Anton Malenkij <itottoro@gmail.com>
2022-06-02 01:52:24 +0200

    create app home create model news add home.html and templates html

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index),
+#    path('/news', NewsList.as_view(), name='news'),
+#    path('', HomeList.as_view(), name='home'),
+#    path('<slug:post_slug>/', show_post, name='post'),
+]
