a46ae4a18d5e4d537de227a046a21ce5e2a792ee
Anton Malenkij <itottoro@gmail.com>
2022-07-04 23:45:44 +0200

    add model photo plug in model photo in gallery

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import News
 from .models import Concerts
-
+from .models import Photo
 
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'created_at', "updated_at", 'is_published')
@@ -12,3 +12,4 @@ class NewsAdmin(admin.ModelAdmin):
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Concerts)
+admin.site.register(Photo)
--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('news', NewsList.as_view(), name='news'),
     path('', HomeList.as_view(), name='home'),
     path('concerts/', ConcertsList.as_view(), name='concerts'),
-    path('about/', index, name='about'),
+    path('about/', AboutList.as_view(), name='about'),
     path('contacts/', index_3, name='contacts'),
     path('<slug:post_slug>/', show_post, name='post'),
 ]
