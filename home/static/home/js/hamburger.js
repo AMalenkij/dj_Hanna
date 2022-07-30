99d187e04d2b305ba9b9ee58a0fe132c4f098f67
Anton Malenkij <itottoro@gmail.com>
2022-07-31 01:49:18 +0200

    refactor css hamburger main news_list refactor html base footer hamburger header about concerts contact detail home home_list

--- home/static/home/js/hamburger.js
+++ home/static/home/js/hamburger.js
@@ -2,13 +2,13 @@ const hamb = document.querySelector("#hamb");
 const popup = document.querySelector("#popup");
 const body = document.body;
 
-// Клонируем меню, чтобы задать свои стили для мобильной версии
+// Clone the menu to set your own styles for the mobile version
 const menu = document.querySelector("#menu").cloneNode(1);
 
-// При клике на иконку hamb вызываем ф-ию hambHandler
+// When clicking on the hamb icon, we call the hambHandler function
 hamb.addEventListener("click", hambHandler);
 
-// Выполняем действия при клике ..
+// actions on click.
 function hambHandler(e) {
   e.preventDefault();
   // Переключаем стили элементов при клике
@@ -18,20 +18,20 @@ function hambHandler(e) {
   renderPopup();
 }
 
-// Здесь мы рендерим элементы в наш попап
+// Here we render elements in a popup
 function renderPopup() {
   popup.appendChild(menu);
 }
 
-// Код для закрытия меню при нажатии на ссылку
+// Code to close the menu when the link is clicked
 const links = Array.from(menu.children);
 
-// Для каждого элемента меню при клике вызываем ф-ию
+// For each menu item, when clicked, we call the function
 links.forEach((link) => {
   link.addEventListener("click", closeOnClick);
 });
 
-// Закрытие попапа при клике на меню
+// Closing the popup when clicking on the menu
 function closeOnClick() {
   popup.classList.remove("open");
   hamb.classList.remove("active");
