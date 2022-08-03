3b94d210894ada553daf2c0c4f89906e638bb624
Anton Malenkij <itottoro@gmail.com>
2022-08-04 00:07:28 +0200

    add TinyMCE text editor

--- config/settings/base.py
+++ config/settings/base.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'home.apps.HomeConfig',
 
     'storages',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
--- home/models.py
+++ home/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from tinymce import models as tinymce_models
 
 
 class News(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     intro = models.TextField(blank=True)
-    content = models.TextField(blank=True)
+    content = tinymce_models.HTMLField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
