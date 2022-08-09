47d88d3b6d59b00b9dbc35d13e112c29695e81bb
Anton Malenkij <itottoro@gmail.com>
2022-08-09 21:20:15 +0200

    change settings static

--- config/urls.py
+++ config/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     prefix_default_language=False
 )
 
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
